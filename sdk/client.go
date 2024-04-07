96024818a3560a2591a23488a7c7713f9c8ef880
Allan Dalton <allan@helloallan.com>
2024-04-07 22:57:09 +1000

    switch to expires_at

--- sdk/client.go
+++ sdk/client.go
@@ -23,7 +23,7 @@ type Client struct {
 type AuthResponse struct {
 	TokenType    string `json:"token_type"`
 	AccessToken  string `json:"access_token"`
-	Expire       int    `json:"expires_in"`
+	Expire       int    `json:"expires_at"`
 	Scope        []int  `json:"scope"`
 	RefreshToken string `json:"refresh_token"`
 }
